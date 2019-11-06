1aee947612663c49c6432c968acca4abf88ca76f
taruc <yansze1998@outlook.com>
2019-11-06 14:45:21 +0800

    first 2.4

--- app/src/main/java/com/example/practical2/MainActivity.kt
+++ app/src/main/java/com/example/practical2/MainActivity.kt
@@ -8,18 +8,43 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.practical2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val myName: MyName = MyName("Aleks Haecky")
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button1 : Button = findViewById(R.id.done_button)
-        button1.setOnClickListener{ buttonClicked(button1)}
+        //val button1 : Button = binding.doneButton
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+
+
     }
 
-    private fun buttonClicked(view : View){
+
+    private fun addNickname(view: View){
+        binding.apply{
+            myName?.nickname = nickname.text.toString()
+            invalidateAll()
+            nickname.visibility=View.GONE
+            doneButton.visibility=View.GONE
+            nicknameText.visibility=View.VISIBLE
+        }
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+    }
+
+    /*private fun buttonClicked(view : View){
         val editText = findViewById<EditText>(R.id.nickname)
         val nicknameView = findViewById<TextView>(R.id.nickname_text)
 
@@ -33,5 +58,5 @@ class MainActivity : AppCompatActivity() {
 
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-    }
+    }*/
 }
